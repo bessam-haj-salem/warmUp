7fa95f0abddfecf0d2d9f7c78406a6ab0a24d0e1
bessam-haj-salem <bessamhajsalem@gmail.com>
2020-03-13 10:25:50 +0100

    i finish

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,32 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+function combine(firstname,lastname){
+	return firstname + ' ' + lastname;
+}
+
+function multiple(n){
+	if(n % 3 === 0){
+		return 'this number is a multiple of 3';
+        return 'this is not a multiple of 3';
+	}
+}
+function average(array){
+	
+	var max = array[0];
+	for( i = 0 ; i < array.length ; i++){
+		if (max < arr[i]){
+			max = arr[i];
+		}
+
+return max;
+
+	}
+
+}
+
+function age(n){
+   return n * 365 * 24 * 60 * 60;
+   }
+   
