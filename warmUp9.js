6eeb89c074e4082866c6a45483da156a11fe4794
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-03-23 10:01:20 +0100

    work done

--- warmUp9.js
+++ warmUp9.js
@@ -4,6 +4,12 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
+ function check(input){
+	return Array.isArray(input);
+	}
+
+// using the method Array.isArray who return a boolean
+
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -14,8 +20,16 @@
 
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
-//    first([1,2,3,4]2)=>[1,2]
+//    first([1,2,3,4],2)=>[1,2]
 
+function elementArr(arr, n){
+	var arr1 = [];
+	for(var i = 1 ; i <= n ; i++ ){
+	arr1.push(i);
+	
+	}
+return arr1;
+}
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
@@ -25,6 +39,10 @@
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+function objReturn(obj){
+	return Object.entries(obj);
+}
+ // // using the method Object.entries who return the arrays of [key, value]
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
