a9fc92f8175743bdadbe8e199e94d591ca62f0cb
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-03-28 09:55:06 +0100

    work done perfectly

--- warmUp14.js
+++ warmUp14.js
@@ -7,3 +7,20 @@
 // array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 // array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 // array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function isSubset(array1,array2){
+	var result = 0;
+	for(var i = 0; i < array1.length; i++){
+		for(var j = 0 ; j < array2.length; j++){ // we must do a counteur inside a counteur so he can scan first all the elements of array with one element of the subarray and splice it after
+			if(array1[i] === array2[j]){
+				result = true;
+				array2.splice(j,1); // splice and go to the next element 
+
+			}
+			else result = false;
+		}
+		result;
+	}
+	return result;
+}
+
